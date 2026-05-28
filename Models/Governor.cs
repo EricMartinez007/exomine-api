ff8e661aa3e8d7ebe5c2f352a96c1b5d29c2fe98
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-05-28 11:48:25 -0500

    Setting up classes and files

---
+++ Models/Governor.cs
@@ -0,0 +1,9 @@
+namespace ExomineAPI.Models;
+
+public class Governor
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int ColonyId { get; set; }
+    public bool IsActive { get; set; }
+}
